07f74b6436f713f92aa9ba30831d91482a64c25a
yandv <allanmxr@gmail.com>
2023-12-02 03:02:21 -0300

    feat: created Login page
    A página de Login foi feita com um template do ChakraUI

---
+++ src/app/login/page.tsx
@@ -0,0 +1,40 @@
+import { Box, Button, Container, FormControl, FormLabel, Heading, Input, Link, Stack, Text } from '@chakra-ui/react';
+
+import { PasswordField } from './PasswordField';
+
+export default function Login() {
+  return (
+    <Container maxW="lg" py={{ base: '12', md: '24' }} px={{ base: '0', sm: '8' }}>
+      <Stack spacing="8">
+        <Stack spacing="6">
+          <Stack spacing={{ base: '2', md: '3' }} textAlign="center">
+            <Heading size={{ base: 'xs', md: 'sm' }}>Log in to your account</Heading>
+            <Text>
+              Não possui uma conta? <Link href="#"></Link>
+            </Text>
+          </Stack>
+        </Stack>
+        <Box
+          py={{ base: '0', sm: '8' }}
+          px={{ base: '4', sm: '10' }}
+          bg={{ base: 'transparent', sm: 'bg.surface' }}
+          boxShadow={{ base: 'none', sm: 'md' }}
+          borderRadius={{ base: 'none', sm: 'xl' }}
+        >
+          <Stack spacing="6">
+            <Stack spacing="5">
+              <FormControl>
+                <FormLabel htmlFor="email">Email</FormLabel>
+                <Input id="email" type="email" />
+              </FormControl>
+              <PasswordField />
+            </Stack>
+            <Stack spacing="6">
+              <Button>Sign in</Button>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Container>
+  );
+}
